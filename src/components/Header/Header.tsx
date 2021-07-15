3eed6047c97c4b4c516f62e9431e9d9d8a9d5814
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-07-14 21:36:44 -0300

    Criado o Header mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
-import { HeaderContainer } from './style';
+import React, { useState, useRef } from 'react';
+import { 
+    HeaderContainer,
+    MenuIcon,
+    HeaderNav,
+    HeaderLi
+} from './style';
+
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+    const [ menuIsOpen, setMenuIsOpen ] = useState(false);
+    
+    const menuList = useRef();
+
+    const toggleShowMenu = () => {
+        const list = menuList.current as HTMLUListElement;
+
+        if(menuIsOpen)
+        {
+            list.style.transform = 'translateY(-100%)';
+        }
+        else
+        {
+            list.style.transform = 'translateY(0%)';
+        }
+
+        setMenuIsOpen(!menuIsOpen);
+    }
+
     return (
         <HeaderContainer>
-            Aqui é um header
+            <MenuIcon
+                icon={faBars}
+                onClick={toggleShowMenu}
+            />
+            <HeaderNav ref={menuList}>
+                <MenuIcon
+                    icon={faTimes}
+                    onClick={toggleShowMenu}
+                />
+                <ul>
+                    <HeaderLi>
+                        Home
+                    </HeaderLi>
+                    <HeaderLi>
+                        Sobre
+                    </HeaderLi>
+                    <HeaderLi>
+                        Contato
+                    </HeaderLi>
+                </ul>
+            </HeaderNav>
         </HeaderContainer>
     )
 }
