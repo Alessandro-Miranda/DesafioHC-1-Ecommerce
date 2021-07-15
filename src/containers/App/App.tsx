f2f03b372036134e8bf755321df5005fe19e2f78
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-07-15 16:07:39 -0300

    Atualizado o App com os componentes utilizados

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
+import Banner from '../../components/Banner/Banner';
+import Header from '../../components/Header';
+import SectionBreak from '../../components/SectionBreak/SectionBreak';
 import { GlobalStyle } from '../../GlobalStyle';
+import Benefits from '../Benefits/Benefits';
+import Categories from '../Categories/Categories';
+import Footer from '../Footer/Footer';
+import Register from '../Register/Register';
 
 const App = () => {  
     return (
         <>
             <GlobalStyle />
             <Header />
+            <Banner />
+            <Benefits />
+            <SectionBreak />
+            <Categories />
+            <SectionBreak />
+            <Register />
+            <Footer />
         </>
     );
 }
