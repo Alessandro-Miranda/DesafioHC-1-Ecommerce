4478a6787824026f695836cba9cc2269e300509d
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-07-15 20:00:18 -0300

    Atualizado o ts-ignore

--- src/containers/Categories/Categories.tsx
+++ src/containers/Categories/Categories.tsx
@@ -5,12 +5,17 @@ import SectionTitles from "../../components/SectionTitles/SectionTitles";
 import { Section, ImagesContainer } from './style';
 
 // @ts-ignore
-import casualMobile from '../../assets/images/casual-mobile.png'; // @ts-ignore
-import casualDesktop from '../../assets/images/casual-desktop.png'; // @ts-ignore
-import femininoMobile from '../../assets/images/feminino-mobile.png'; // @ts-ignore
-import femininoDesktop from '../../assets/images/feminino-desktop.png'; // @ts-ignore
-import masculinoMobile from '../../assets/images/masculino-mobile.png'; // @ts-ignore
-import masculinoDesktop from '../../assets/images/masculino-desktop.png';
+import casualMobile from '../../assets/images/casual-mobile.png';
+// @ts-ignore
+import casualDesktop from '../../assets/images/casual-desktop.png';
+// @ts-ignore
+import femaleMobile from '../../assets/images/feminino-mobile.png';
+// @ts-ignore
+import femaleDesktop from '../../assets/images/feminino-desktop.png';
+// @ts-ignore
+import maleMobile from '../../assets/images/masculino-mobile.png';
+// @ts-ignore
+import maleDesktop from '../../assets/images/masculino-desktop.png';
 
 const Categories = () => {
     const informations = {
@@ -27,14 +32,14 @@ const Categories = () => {
         },
         {
             category: "feminino",    
-            srcSetMobile: femininoMobile,
-            srcSetDesktop: femininoDesktop,
+            srcSetMobile: femaleMobile,
+            srcSetDesktop: femaleDesktop,
             alt: "Modelo feminina utilizando casaco de frio"
         },
         {
             category: "masculino",    
-            srcSetMobile: masculinoMobile,
-            srcSetDesktop: masculinoDesktop,
+            srcSetMobile: maleMobile,
+            srcSetDesktop: maleDesktop,
             alt: "Modelo masculino utilizando casaco de frio"
         }
     ];
