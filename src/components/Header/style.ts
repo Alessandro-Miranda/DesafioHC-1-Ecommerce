52839314350d87c18577d3220a2b295c51893fbe
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-07-14 21:37:29 -0300

    Criado o style do header

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,12 +1,47 @@
 import style from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const HeaderContainer = style.header`
-    font-size: ${props => props.theme.fontSize};
-    color: ${props => props.theme.secundaryColor};
-    grid-column: 7;
-    background: red;
+    height: max-content;
+    padding: .875rem 1.875rem;
+    position: relative;
 `;
 
-export const HeaderItem = style.div`
-    
+export const MenuIcon = style(FontAwesomeIcon)`
+    font-size: 2.175rem;
+    color: var(--finishColor);
+    display: block;
+    margin-left: auto;
+
+    @media screen and (min-width: 700px)
+    {
+        display: none;
+    }
+`;
+
+export const HeaderNav = style.nav`
+    display: block;
+    background: var(--finishColor);
+    width: 100%;
+    height: max-content;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: transform .3s ease-in-out;
+    transform: translateY(-100%); 
+    svg {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        color: var(--primaryColor);
+        font-size: 1.575rem;
+    }
+`;
+
+export const HeaderLi = style.li`
+    text-transform: uppercase;
+    text-align: center;
+    padding: 20px 0;
+    font-weight: 600;
+    border-bottom: 1px solid var(--primaryColor);
 `;
