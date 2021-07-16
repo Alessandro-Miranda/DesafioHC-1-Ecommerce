49821ae02f33ab954dc040a415fafaa132334f8e
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-07-16 13:09:20 -0300

    Criado o breakpoint para desktop

--- src/containers/Benefits/style.ts
+++ src/containers/Benefits/style.ts
@@ -6,4 +6,30 @@ export const Section = style.section`
     flex-direction: column;
     justify-content: center;
     margin: 30px auto 0 auto;
+
+    @media screen and (min-width: 700px)
+    {
+        width: 100%;
+        margin: 100px auto 0 auto;
+
+        & > h1
+        {
+            font-size: 24px;
+
+            & > span
+            {
+                transform: translateY(-400%);
+            }
+        }
+    }
+`;
+
+export const CardsContainer = style.div`
+    @media screen and (min-width: 700px)
+    {
+        display: flex;
+        flex-direction: row;
+        margin-top: 72px;
+        margin-bottom: 120px;
+    }
 `;
