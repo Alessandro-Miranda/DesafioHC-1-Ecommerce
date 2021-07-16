fd64e0abff59d0e7a74a4ba2477af64dca412b9f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-07-16 11:58:03 -0300

    Finalizado o banner versão desktop

--- src/containers/MainBanner/MainBanner.tsx
+++ src/containers/MainBanner/MainBanner.tsx
@@ -13,10 +13,51 @@ const Section = style.section`
     align-items: center;
     margin-top: 50px;
 
+    & aside
+    {
+        display: none;
+    }
+    
     @media screen and (min-width: 700px)
     {
         flex-direction: row;
         max-width: 100%;
+        justify-content: flex-start;
+        margin-top: 80px;
+
+        & aside
+        {
+            justify-self: flex-end;
+            position: relative;
+        }
+
+        & aside svg:nth-child(2)
+        {
+            margin-top: 40px;
+            margin-bottom: 40px;
+        }
+        & aside::before
+        {
+            content: '';
+            position: absolute;
+            top: -105px;
+            right: -55px;
+            width: 139px;
+            height: .5px;
+            background: var(--finishColor);
+            transform: rotate(90deg);
+        }
+        & aside::after
+        {
+            content: '';
+            position: absolute;
+            bottom: -105px;
+            right: -55px;
+            width: 139px;
+            height: .5px;
+            background: var(--finishColor);
+            transform: rotate(90deg);
+        }
     }
 `;
 
