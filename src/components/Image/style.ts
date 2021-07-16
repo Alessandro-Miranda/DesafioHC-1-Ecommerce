25b885ed102f8609b935cc773199d2c829fd4248
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-07-16 16:06:46 -0300

    iniciado a estilização para desktop

--- src/components/Image/style.ts
+++ src/components/Image/style.ts
@@ -2,14 +2,47 @@ import style from 'styled-components';
 
 export const Div = style.div`
     max-width: 100%;
-    position: relative;margin-top: 15px;
+    width: max-content;
+    position: relative;
+    margin-top: 15px;
+
+    @media screen and (min-width: 700px)
+    {
+        margin-top: 62px;
+        &:first-of-type
+        {
+            grid-column-start: 1;
+            grid-column-end: 6;
+            grid-row-start: 1;
+        }
+
+        &:nth-child(2)
+        {
+            grid-column-start: 6;
+            grid-column-end: 15;
+            grid-row-start: 1;
+            grid-row-end: 4
+        }
+
+        &:nth-child(3)
+        {
+            grid-column-start: 6;
+            grid-column-end: 15;
+            grid-row-start: 4;
+        }
+
+        & > picture:nth-child(2)
+        {
+            background: blue;
+        }
+    }
 `;
 
 export const Picture = style.picture`
     position: relative;
     max-width: 414px;
     width: 393px;
-    max-hieght: 350px;
+    max-height: 350px;
     height: 350px;
     max-height: max-content;
 
@@ -20,10 +53,18 @@ export const Picture = style.picture`
         top: 0;
         max-width: 414px;
         width: 393px;
-        max-hieght: 350px;
+        max-height: 350px;
         height: 350px;
         background: #030303;
         opacity: .7;
+    }
+
+    @media screen and (min-width: 700px)
+    {
+        max-width: 100%;
+        width: max-content;
+        max-height: 100%;
+        height: auto;
     }
 `;
 
@@ -32,6 +73,22 @@ export const Img = style.img`
     max-width: 100%;
     width: 393px;
     height: max-content;
+
+    @media screen and (min-width: 700px)
+    {
+        width: max-content;
+        height: auto;
+
+        &:first-of-type
+        {
+            width: 405px;
+        }
+
+        &:nth-child(2), &:nth-child(3)
+        {
+            width: 800px;
+        }
+    }
 `;
 
 export const Span = style.span`
