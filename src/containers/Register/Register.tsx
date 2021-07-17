c536cec32cc4bd3a93f34eec3ceab69de63ca0b9
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-07-16 21:42:40 -0300

    Criado breakpoint para desktop

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -6,6 +6,21 @@ const Section = style.section`
     max-width: 95%;
     text-align: center;
     margin: 0 auto;
+
+    @media screen and (min-width: 700px)
+    {
+        margin: 150px auto 0 auto;
+
+        & section.registerBreakSection
+        {
+            max-width: 720px;
+            margin: 0 auto;
+        }
+        & section.registerBreakSection h3
+        {
+            font-size: 23px;
+        }
+    }
 `;
 
 const theme = {
@@ -16,9 +31,12 @@ const Register = () => {
     return (
         <Section>
             <ThemeProvider theme={theme}>
-                <Form {...{
-                    formTitle: "não fique de fora das nossas promoções"
-                }} />
+                <Form {
+                    ...{
+                        formTitle: "não fique de fora das nossas promoções",
+                        className: 'registerBreakSection'
+                    }
+                } />
             </ThemeProvider>
         </Section>
     );
